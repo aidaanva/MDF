3c7f1a2e8769b97ea5e9fe8ba9b6a1857f616185
aidaanva <aida.andrades@gmail.com>
2021-05-04 10:27:20 +0200

    Made the excluding function optional, fixed names starting with numbers, changed output name to include the percentage for the partial deletion

--- MDF.R
+++ MDF.R
@@ -7,7 +7,7 @@ parser <- argparser::arg_parser( 'Produces a fasta file allowing the percentage 
 parser <- add_argument(parser, 'input',
                        type='character',
                        nargs=1,
-                       help='Path to the snpTable.tsv produced by MultiVCFAnalyzer')
+                       help='Path to the snpTable.tsv produced by MultiVCFAnalyzer or file.fasta. Note: if file.fasta is used as input, the flag --fasta must be included for proper parsing')
 parser <- add_argument(parser, 'percentage',
                        type="integer",
                        nargs=1,
@@ -16,10 +16,10 @@ parser <- add_argument(parser, 'output',
                        type='character',
                        nargs=1,
                        help='Path to output directory')
-parser <- add_argument(parser, 'toExclude',
+parser <- add_argument(parser, '--toExclude',
                        type = 'character',
-                       nargs=1,
-                       help='File containing genomes to exclude one per line')
+                       help = 'Path to file containing genomes to exclude one per line',
+                       default = 'None')
 parser <- add_argument(parser, '--fasta',
                        type = 'logical',
                        help = 'Use when input is a fasta file',
@@ -29,26 +29,37 @@ argv <- parse_args(parser)
 library(tidyverse)
 library(seqinr)
 
-partialDeletionFasta <- function(input, df, percentage, outputpath, genomeToExclude) {
-  toExclude <- read.delim(genomeToExclude, header = F)
+excludingGenomes <- function(df, genometoExclude) {
+  print(genometoExclude)
+  toExclude <- read.delim(genometoExclude, header = F)
   df_sampleRows <- df %>%
     filter(!Genome %in% toExclude$V1)
-  print("Table done")
-  partialDelInfo <- df_sampleRows %>%
+  print(paste("Provided genomes", toExclude$V1, "have been excluded", sep = " "))
+  return(df_sampleRows)
+}
+
+partialDeletionFasta <- function(input, inputdf, percentage, outputpath, genomeToExclude) {
+  if( genomeToExclude == "None"){
+    df <- inputdf
+    print("No genomes excluded")
+  } else {
+    df <- excludingGenomes(inputdf, genomeToExclude)
+  }
+  partialDelInfo <- df %>%
     group_by(Position) %>%
     summarise(missing=sum(Call== "N"), total=n()) %>%
     mutate(percent = (missing/total)*100) %>%
     filter(percent <= percentage)
   print("Missing information calculated")
   if( input == "fasta") {
-    snpTable_pd <- df_sampleRows %>%
+    snpTable_pd <- df %>%
       filter(Position %in% partialDelInfo$Position) %>%
       mutate(Call = as.character(Call)) %>%
       mutate(Final_Call = if_else(Call == '?'| Call == '-' , 'N' , Call)) %>%
       select(-Call) %>%
       pivot_wider(names_from = Position, values_from = Final_Call)
   } else {
-    snpTable_pd <- df_sampleRows %>%
+    snpTable_pd <- df %>%
       filter(Position %in% partialDelInfo$Position) %>%
       mutate(Call = as.character(Call), Ref = as.character(Ref)) %>%
       mutate(Final_Call = if_else(Call == '.', Ref, Call)) %>%
@@ -56,13 +67,12 @@ partialDeletionFasta <- function(input, df, percentage, outputpath, genomeToExcl
       pivot_wider(names_from = Position, values_from = Final_Call)
   }
 
-  pd <- 100-percentage
 
-  outputTable <- paste(outputpath, pd, "pd.tsv", sep = "_")
+  outputTable <- paste(outputpath, percentage, "pd.tsv", sep = "_")
 
   write_tsv(snpTable_pd, outputTable, col_names = F)
   print("snpTable_pd saved")
-  outputFasta <- paste(outputpath, pd, "pd.fasta", sep = "_")
+  outputFasta <- paste(outputpath, percentage, "pd.fasta", sep = "_")
   system(paste("awk '{print \">\"$1; $1=\"\"; print $0}' OFS=", outputTable, ">", outputFasta))
   write_tsv(snpTable_pd, outputTable, col_names = T)
   print("Fasta_dp saved")
@@ -82,7 +92,7 @@ fastatoTibble <- function(fasta) {
 }
 
 snpTabletoTibble <- function(snpTable) {
-  df <- read.delim(snpTable)
+  df <- read.delim(snpTable, stringsAsFactors = F, check.names = F)
   df_sampleRows <- df %>%
     pivot_longer(-contains(c("Position","Ref")), names_to = "Genome", values_to = "Call")
   return(df_sampleRows)
@@ -92,13 +102,14 @@ snpTabletoTibble <- function(snpTable) {
 
 inFasta=argv$fasta
 print(inFasta)
+print(argv$toExclude)
 
 if( argv$fasta == T ){
   print("Fasta as input, start run with fasta mode")
   fastaT <- fastatoTibble(fasta = argv$input)
-  partialDeletionFasta(input = "fasta", df = fastaT, percentage = argv$percentage, outputpath = argv$output, genomeToExclude = argv$toExclude)
+  partialDeletionFasta(input = "fasta", inputdf = fastaT, percentage = argv$percentage, outputpath = argv$output, genomeToExclude = argv$toExclude)
 } else {
   print("SnpTable as input, start run with table mode")
   snpTableT <- snpTabletoTibble(snpTable = argv$input)
-  partialDeletionFasta(input = "snpTable", df = snpTableT, percentage = argv$percentage, outputpath = argv$output, genomeToExclude = argv$toExclude)
+  partialDeletionFasta(input = "snpTable", inputdf = snpTableT, percentage = argv$percentage, outputpath = argv$output, genomeToExclude = argv$toExclude)
 }
